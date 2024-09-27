4101f8d93dded21fffb8bc391bfc4d87075c1a0e
wilort <97401424+wilort@users.noreply.github.com>
2024-09-27 22:16:39 +0000

    wip

--- src/solver.py
+++ src/solver.py
@@ -2,7 +2,7 @@ import random
 from typing import Tuple, List
 import pulp
 
-from src.mastermind import Colors
+from src.mastermind import Colors, PegColors
 
 class MastermindSolver:
 
@@ -19,17 +19,16 @@ class MastermindSolver:
 
     def solve(self, quiet=True) -> Tuple[List[Colors], int]:
 
-        # create random guess
-        guess = self.create_guess()
         iterations = 0
 
-        # get hint
+        guess = self.create_guess()
+
         hint = self.mastermind.get_hint(guess)
 
-        if hint == [Colors.RED]*self.mastermind.code_length:
-            return guess
+        if all(c == PegColors.RED for c in hint):
+            return guess, iterations
 
-        ball_ids = range(1, self.mastermind.code_length + 1)
+        ball_ids = range(self.mastermind.code_length)
         color_ids = list(Colors)
 
         # the variable x[b][c] is 1 iff ball b has color c
@@ -43,62 +42,80 @@ class MastermindSolver:
                                 cat = "Binary")
         constraints = []
 
-        # create the general constraint:
-        # Total numbers of balls should be equal to the code length
-        num_chosen_balls = pulp.lpSum(x[b][c] for b in ball_ids for c in color_ids)
-        constraint = num_chosen_balls == self.mastermind.code_length, ""
-        constraints.append(constraint)
+        # create some general constraints:
+        # 1. each ball position has exactly one color
+        for b in ball_ids:
+            constraint = pulp.lpSum(x[b][c] for c in color_ids) == 1, ""
+            constraints.append(constraint)
 
+        # 2. force binary y[c] to be 1 if the color is used in any ball position
         for c in color_ids:
             constraint = y[c] <= pulp.lpSum(x[b][c] for b in ball_ids), ""
             constraints.append(constraint)
-            constraint = pulp.lpSum(x[b][c] for b in ball_ids) <= 4*y[c], ""
+            constraint = pulp.lpSum(x[b][c] for b in ball_ids) <= 100000*y[c], ""
             constraints.append(constraint)
 
-
-        while hint != [Colors.RED]*self.mastermind.code_length:
+        while any(h != PegColors.RED for h in hint):
             iterations += 1
 
+            if iterations > 100:
+                raise Exception("Too many iterations")
+
             prob = pulp.LpProblem("mastermind_problem", pulp.LpMinimize)
-            self.prob += 0, "Objective_Function"
+            prob += x[b][Colors.RED], "Objective_Function" # for some reason we need to set this to something. not sure why?
+
 
             # color red represents the number of correct ball colors in the correct position
-            num_balls_in_correct_position = hint.count(Colors.RED)
+            num_balls_in_correct_position = hint.count(PegColors.RED)
             if num_balls_in_correct_position > 0:
                 constraint = pulp.lpSum(x[b][c] for b,c in enumerate(guess)) == num_balls_in_correct_position , ""
                 constraints.append(constraint)
 
 
             # color none represents the number of ball colors not in the solution at all
-            num_balls_not_in_solution = hint.count(Colors.NONE)
+            num_balls_not_in_solution = hint.count(PegColors.NONE)
             if num_balls_not_in_solution > 0:
                 constraint = pulp.lpSum(1 - y[c] for c in guess) == num_balls_not_in_solution , ""
                 constraints.append(constraint)
 
-            # num white represents the number of correct ball colors but in the wrong position
-            num_balls_in_wrong_position = hint.count(Colors.WHITE)
-            if num_balls_in_wrong_position > 0:
 
-                # if x balls are in the wrong position we know that the inverse sum of the balls will b x
+            # num white represents the number of correct ball colors but in the wrong position
+            num_balls_in_wrong_position = hint.count(PegColors.WHITE)
+            if num_balls_in_wrong_position > 0:
                 constraint = pulp.lpSum(1 - x[b][c] for b,c in enumerate(guess)) == num_balls_in_wrong_position , ""
                 constraints.append(constraint)
 
+
             # add the constraints
-            prob += constraints
+            for c in constraints:
+                prob += c
+
+
+            prob.writeLP(f"tmp/model_{iterations}.lp")
 
             # solve the problem
             if quiet:
-                prob.solve(pulp.PULP_CBC_CMD(timeLimit=60, msg=False))
+                prob.solve(solver=pulp.PULP_CBC_CMD(timeLimit=60, msg=False))
             else:
                 prob.solve()
 
+            status = pulp.LpStatus[prob.status]
+
+            # # reality check
+            # for b in ball_ids:
+            #     if sum(x[b][c].varValue for c in color_ids) != 1:
+            #         lol = [x[b][c].varValue for c in color_ids]
+            #         raise Exception(f"Reality check failed for ball {b}. Values: {lol}")
+
+            if status != "Optimal":
+                raise Exception(f"No optimal solution found. Status: {status}")
+
             # get the new guess
-            
-            new_guess = []
+            new_guess = self.mastermind.code_length*[None]
             for b in ball_ids:
                 for c in color_ids:
                     if x[b][c].varValue == 1:
-                        new_guess.append(c)
+                        new_guess[b] = c
 
             guess = new_guess
             hint = self.mastermind.get_hint(guess)
--- tests/test_solver.py
+++ tests/test_solver.py
@@ -11,9 +11,29 @@ def mastermind():
         mastermind.new_game()
         return mastermind
 
-def test_solver(mastermind):
-    solver = MastermindSolver(mastermind)
-    solution, num_iterations = solver.solve()
-    assert len(solution) == mastermind.code_length
-    assert solution == mastermind.solution
+def test_solver_correct_first_guess(mastermind):
 
+    solver = MastermindSolver(mastermind)
+
+    mocked_first_guess = [Colors.RED, Colors.BLUE, Colors.GREEN, Colors.YELLOW]
+
+    with mock.patch.object(MastermindSolver, "create_guess", return_value=mocked_first_guess):
+        solution, num_iterations = solver.solve()
+        assert mastermind.check_solution(solution)
+
+def test_solver_incorrect_first_guess(mastermind):
+
+    solver = MastermindSolver(mastermind)
+
+    mocked_first_guess = [Colors.RED, Colors.BLUE, Colors.GREEN, Colors.ORANGE]
+
+    with mock.patch.object(MastermindSolver, "create_guess", return_value=mocked_first_guess):
+        solution, num_iterations = solver.solve()
+        assert mastermind.check_solution(solution)
+
+def test_solver(mastermind):
+
+    solver = MastermindSolver(mastermind)
+
+    solution, num_iterations = solver.solve()
+    assert mastermind.check_solution(solution)
